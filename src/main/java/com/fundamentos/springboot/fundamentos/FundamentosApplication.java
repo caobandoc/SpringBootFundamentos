41d7c3553c4bbf47e27e45b988b6c83f3f9da541
caobandoc <obandocarlosandres@gmail.com>
2022-04-11 22:21:52 -0500

    Uso de properties y valores

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,6 +2,7 @@ package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDepenedency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -14,12 +15,14 @@ public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDepenedency componentDepenedency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
+	private MyBeanWithProperties myBeanWithProperties;
 
 	public FundamentosApplication(@Qualifier("componentImplementTwo") ComponentDepenedency componentDepenedency, MyBean myBean,
-								  MyBeanWithDependency myBeanWithDependency) {
+								  MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties) {
 		this.componentDepenedency = componentDepenedency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
 	}
 
 	public static void main(String[] args) {
@@ -31,5 +34,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDepenedency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
+		System.out.println(myBeanWithProperties.function());
 	}
 }
